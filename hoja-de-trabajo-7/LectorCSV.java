65143bf495f943d43432e7fea046523b80b54f59
AntonySAZ <dantony.saz7@gmail.com>
2025-03-17 18:39:55 -0600

    manejo del csv

--- hoja-de-trabajo-7/LectorCSV.java
+++ hoja-de-trabajo-7/LectorCSV.java
@@ -3,7 +3,21 @@ import java.util.*;
 
 public class LectorCSV {
     
-    public static void cargarProducto(){
+    public static void cargarProducto(String archivo, BinarySearchTree<String, Producto> skuTree, BinarySearchTree<String, Producto> nameTree){
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            br.readLine();
+            while ((linea = br.readLine()) != null) {
+                String[] values = linea.split(",");
+                if(values.length ==4){
+
+                    Producto producto = new Producto(values[0], values[1], values[2], values[3]);
+                    skuTree.insert(producto.sku, producto);
+                    nameTree.insert(producto.nombre, producto);
+            }}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
